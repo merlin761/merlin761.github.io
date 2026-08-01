f50ce9f70b9b05514a1b25a8b987b5c0ad2d5507
mel <71297825+merlin761@users.noreply.github.com>
2026-07-31 23:09:45 -0400

    Change footer logo from 'Vinay' to 'Merlin'

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,13 @@
 import { html } from "https://unpkg.com/lit-html?module";
 import { mount } from "../utils/dom.js";
 import { contact, footer } from "../../user-data/data.js";
-
 const copyrightText = footer.find((item) => item.label === "copyright-text")?.data?.[0] ?? "";
-
 const footerTemplate = () => html`
   <div class="container footer-inner">
     <div class="footer-brand">
-      <p class="footer-logo">Vinay<span class="gradient-text">.</span></p>
+      <p class="footer-logo">Merlin<span class="gradient-text">.</span></p>
       <p class="footer-tagline">${copyrightText}</p>
     </div>
-
     <ul class="footer-links">
       ${contact.map(
         (item) => html`
@@ -22,7 +19,6 @@ const footerTemplate = () => html`
         `
       )}
     </ul>
-
     <div class="footer-meta">
       <p class="footer-visitors">
         <span class="visitor-label">Visitors</span>
@@ -32,7 +28,6 @@ const footerTemplate = () => html`
     </div>
   </div>
 `;
-
 export function mountFooter() {
   return mount("footer", footerTemplate());
 }
